0c165c46d288a75d659e8fa238720ae3728c68db
Eerikkie <kaveringleb@gmail.com>
2021-11-14 17:29:41 +0300

    addefasdfasdasdf

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
