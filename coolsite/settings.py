7f2229cdc3915517f5355c2d8618f143ee59c797
Eerikkie <kaveringleb@gmail.com>
2021-11-15 15:47:57 +0300

    zxcommit2

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
